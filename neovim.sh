7cfd3aca8cb6c23b661cc7134d70c2319377d283
Dhaval Patel <dhavalpatel32768@gmail.com>
2021-01-27 20:47:50 +0530

    Added Vimplug and oh-my-zsh

--- neovim.sh
+++ neovim.sh
@@ -4,5 +4,5 @@ printProgress "Installing dependencies"
 carbonInstall neovim nodejs fzf ripgrep
 printProgress "Restoring Configuration"
 carbonClone https://github.com/dhaval15/nvim.git $CARBON_INSTALLATION_SCRIPT/.config/nvim
-# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
--- shell.sh
+++ shell.sh
@@ -2,10 +2,10 @@
 echo "$CARBON_INSTALLATION_DIRECTORY"
 printSection "Shell"
 printProgress "Installing dependencies"
-carbonInstall zsh zsh-completions
+carbonInstall zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
 printProgress "Cloning scripts"
 carbonClone https://github.com/dhaval15/scripts.git $CARBON_INSTALLATION_DIRECTORY/scripts
 printProgress "Setting up ohmyzsh"
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 printProgress "Restoring zsh configuration"
 carbonLink $CARBON_INSTALLATION_DIRECTORY/dots/.zshrc $CARBON_INSTALLATION_DIRECTORY/.zshrc
