d953904791f1431474079c2f6513ac1fbc5d2fd7
Dhaval <dhavalpatel32768@gmail.com>
2021-01-27 23:01:49 +0530

    added menu generator

--- desktop.sh
+++ desktop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 printSection "Desktop"
 printProgress "Installing dependencies"
-carbonInstall openbox awesome-git polybar rofi-git plank compton-tryone-git lightdm-webkit-theme-aether nitrogen
+carbonInstall openbox obmenu-generator awesome-git polybar rofi-git plank compton-tryone-git lightdm-webkit-theme-aether nitrogen
 printProgress "Restoring Configuration"
 carbonLink $CARBON_INSTALLATION_DIRECTORY/dots/openbox $CARBON_INSTALLATION_DIRECTORY/.config/openbox
 carbonLink $CARBON_INSTALLATION_DIRECTORY/dots/awesome $CARBON_INSTALLATION_DIRECTORY/.config/awesome
